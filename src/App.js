46c64d12066613004fa760227de96977586220c0
Casta <palolahrn0104@gmail.com>
2020-07-31 14:15:52 -0500

    Cree la tabla en app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tabla de Integrantes</h1>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Apellidos</th>
+              <th>Correos</th>
+              <th>Teléfono</th>
+              <th>Tipo Documento</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Paola Andrea</td>
+              <td>Hernández Velásquez</td>
+              <td>cmtecnica09@elpoli.edu.co</td>
+              <td>33093909043</td>
+              <td>119198998</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>amet</td>
+              <td>consectetur</td>
+              <td>adipiscing</td>-+
+              <td>elit</td>
+            </tr>
+            <tr>
+              <td>1,003</td>
+              <td>Integer</td>
+              <td>nec</td>
+              <td>odio</td>
+              <td>Praesent</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
